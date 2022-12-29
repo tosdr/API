cbe6c209ef3003a44078867740f0ef17547b7776
Justin Back <j.back@jrbit.de>
2022-12-29 18:21:52 +0000

    Fix createSpamEntry

--- functions/Spam/DELETE/v1/src/helpers/Phoenix.ts
+++ functions/Spam/DELETE/v1/src/helpers/Phoenix.ts
@@ -5,7 +5,7 @@ export class Phoenix {
 
   
   static async createSpamEntry(SpammableType: string, id: number, client: Client): Promise<any> {
-    return (await client.query("INSERT INTO spams (spammable_type, spammable_id, flagged_by_admin_or_curator, cleaned) VALUES ($1::string, $2::integer, true, false)", [SpammableType, id]));
+    return (await client.query("INSERT INTO spams (spammable_type, spammable_id, flagged_by_admin_or_curator, cleaned, created_at, updated_at) VALUES ($1::text, $2::integer, true, false, now(), now())", [SpammableType, id]));
   }
 
 
