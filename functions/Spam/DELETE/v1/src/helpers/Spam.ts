bf9f6af69f677146954179955d2c38c93a43f169
Justin René Back <j.back@jrbit.de>
2023-01-11 18:57:27 +0000

    Use S3 for functions

--- functions/Spam/DELETE/v1/src/helpers/Spam.ts
+++ functions/Spam/DELETE/v1/src/helpers/Spam.ts
@@ -1,31 +1,50 @@
 import { Client, Storage, InputFile } from 'node-appwrite';
+import * as Minio from 'minio';
+import * as fs from 'fs';
 
 
 export class Spam {
 
   
   static async loadClassifier(): Promise<string> {
-    let appwriteClient = new Client()
-    .setEndpoint(process.env.APPWRITE_HOSTNAME)
-    .setProject(process.env.APPWRITE_PROJECT_KEY)
-    .setKey(process.env.APPWRITE_API_KEY);
 
 
-    let storage = new Storage(appwriteClient);
-    return (await (storage.getFileDownload(process.env.APPWRITE_BUCKET_ID, "bayes_spam_classifier"))).toString('utf-8'); 
+    var minioClient = new Minio.Client({
+      endPoint: process.env.S3_HOST,
+      port: 443,
+      useSSL: true,
+      accessKey: process.env.S3_ACCESS,
+      secretKey: process.env.S3_SECRET
+    });
+
+     await minioClient.fGetObject(process.env.S3_BUCKET, "bayes_spam_classifier.json", "/tmp/bayes_spam_classifier.json");
+
+     let data = fs.readFileSync("/tmp/bayes_spam_classifier.json").toString();
+
+     fs.unlinkSync("/tmp/bayes_spam_classifier.json");
+
+     return data;
   }
 
 
   static async saveClassifier(data: string): Promise<boolean> {
-    let appwriteClient = new Client()
-    .setEndpoint(process.env.APPWRITE_HOSTNAME)
-    .setProject(process.env.APPWRITE_PROJECT_KEY)
-    .setKey(process.env.APPWRITE_API_KEY);
 
 
-    let storage = new Storage(appwriteClient);
-    await storage.deleteFile(process.env.APPWRITE_BUCKET_ID, "bayes_spam_classifier");
-    return (await (storage.createFile(process.env.APPWRITE_BUCKET_ID, "bayes_spam_classifier", InputFile.fromPlainText(data, "bayes_spam_classifier.json")))).$id !== null; 
+    var minioClient = new Minio.Client({
+      endPoint: process.env.S3_HOST,
+      port: 443,
+      useSSL: true,
+      accessKey: process.env.S3_ACCESS,
+      secretKey: process.env.S3_SECRET
+    });
+
+     fs.writeFileSync("/tmp/bayes_spam_classifier.json", data);
+
+     await minioClient.fPutObject(process.env.S3_BUCKET, "bayes_spam_classifier.json", "/tmp/bayes_spam_classifier.json");
+
+     fs.unlinkSync("/tmp/bayes_spam_classifier.json");
+
+     return true;
   }
 
 }
