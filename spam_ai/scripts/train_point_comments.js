277b6d9cded451171cb9d93420d2174cb84f6aa4
Justin René Back <j.back@jrbit.de>
2023-01-11 16:12:51 +0000

    Add Helper script

---
+++ spam_ai/scripts/train_point_comments.js
@@ -0,0 +1,67 @@
+const natural = require('natural');
+const sdk = require('node-appwrite');
+const pg = require('pg');
+const striphtml = require("string-strip-html");
+const axios = require("axios");
+const Amqp = require('amqp-ts');
+
+let sql =`SELECT SUMMARY
+FROM POINT_COMMENTS
+INNER JOIN USERS ON POINT_COMMENTS.USER_ID = USERS.ID
+WHERE (USERS.ADMIN = TRUE
+							OR USERS.CURATOR = TRUE)
+	AND TRIM(SUMMARY) != 'approved:'
+	AND TRIM(SUMMARY) != 'declined:'
+	AND TRIM(SUMMARY) != 'changes-requested:'
+	AND TRIM(SUMMARY) != 'Verified.'
+	AND TRIM(SUMMARY) != 'Confirmed.'
+	AND TRIM(SUMMARY) != 'Duplicate.'
+	AND TRIM(SUMMARY) != 'approved: without comment'
+	AND TRIM(SUMMARY) != 'approved: Point approved.'
+	AND TRIM(SUMMARY) != 'declined: duplicate';`;
+
+
+async function init(){
+
+
+const client = new pg.Client();
+await client.connect();
+
+
+let result = (await client.query(sql)).rows;
+
+var connection = new Amqp.Connection(process.env.AMQP_URL);
+var queue = connection.declareQueue('spam_train', {durable: true});
+
+result.forEach(async (row) => {
+
+    let filtered = striphtml.stripHtml(row.summary).result.replace("APPROVED", "").replace("CHANGES REQUESTED", "").replace("DECLINED", "").trim();
+
+
+    if(filtered == "No comment given" || filtered == "CHANGES REQUESTED" || filtered == ""){
+        return;
+    }
+
+
+    var message = new Amqp.Message(JSON.stringify({
+        "text": filtered,
+        "type": "ham"
+      }));
+      queue.send(message);
+      
+    console.log("Sent message");
+
+});
+
+await client.end();
+await queue.close();
+await connection.close();
+
+
+
+
+
+
+}
+
+init();
