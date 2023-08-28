4f89e6a62b3580287806e000f585aff6ed22da88
Madeline OLeary <madeline.w.oleary@gmail.com>
2023-08-28 16:18:26 +0200

    adds helpers, returns all document IDs if no ID given in params

--- functions/Document/GET/v1/src/index.ts
+++ functions/Document/GET/v1/src/index.ts
@@ -14,30 +14,74 @@
 import { Client } from 'pg';
 import {Bitmask, Document, RESTfulAPI} from "api-microservices";
 import { Phoenix } from './helpers/Phoenix';
-import Flagsmith from 'flagsmith-nodejs';
 
 module.exports = async function (req: any, res: any) {
 
   const client = new Client()
   await client.connect();
 
-  const flagsmith = new Flagsmith({
-    environmentKey: process.env.FLAGSMITH_KEY,
-    apiUrl: process.env.FLAGSMITH_HOSTNAME,
-  });
-  const flags = await flagsmith.getEnvironmentFlags();
-
   let request = JSON.parse(req.payload);
   if(request.id) {
-    if (!await Phoenix.caseExists(request.id, client)) {
+    if (!await Phoenix.documentExists(request.id, client)) {
       await client.end();
       return res.json(RESTfulAPI.response(Bitmask.INVALID_PARAMETER, "The Document does not exist", []), 404);
     }
-    let docObj = await Phoenix.getDocument(request.id, client);
+    let docObj = await Phoenix.getDocumentById(request.id, client);
     await client.end();
     res.json(RESTfulAPI.response(Bitmask.REQUEST_SUCCESS, "OK", Document.v1.fromRow(docObj).toObject()));
     return res.json();
-  } else {
-    return res.json(RESTfulAPI.response(Bitmask.INVALID_PARAMETER, "Missing Document ID", []), 400);
   }
+
+  let totalDocuments = await Phoenix.countAllDocuments(client);
+  let documentsPerPage = 100;
+  let currentPage = Number.parseInt(request.page ?? 1);
+
+  if(!Number.isInteger(currentPage)){
+    await client.end();
+    return res.json(RESTfulAPI.response(Bitmask.INVALID_PARAMETER, "The Page Parameter is not a number!", {
+      page: currentPage
+    }), 400);
+  }
+
+  /* Check if page is zero or below */
+  if(currentPage < 1){
+    currentPage = 1;
+  }
+  
+  let _calculatedPages = Math.ceil(totalDocuments / documentsPerPage);
+  /* If page is one then offset is zero */
+  let _calculatedOffset = (currentPage === 1 ? 0 : ((currentPage - 1) * documentsPerPage));
+
+  console.log("totalDocuments", totalDocuments);
+  console.log("documentsPerPage", documentsPerPage);
+  console.log("currentPage", currentPage);
+  console.log("_calculatedPages", _calculatedPages);
+  console.log("_calculatedOffset", _calculatedOffset);
+
+  if(currentPage > _calculatedPages){
+    await client.end();
+    return res.json(RESTfulAPI.response(Bitmask.INVALID_PARAMETER, "The Page Parameter is out of range!", {
+      page: currentPage
+    }), 400);
+  }
+
+  let phoenixDocuments = await Phoenix.getAllDocumentsOffset(documentsPerPage, _calculatedOffset, client);
+
+  let documentSkeleton: any = [];
+
+  phoenixDocuments.forEach((document: any) => {
+    documentSkeleton.push(document.id).toObject();
+  });
+  
+  await client.end();
+
+  return res.json(RESTfulAPI.response(Bitmask.REQUEST_SUCCESS, "All documents below", {
+    _page: {
+      total: totalDocuments,
+      current: currentPage,
+      start: 1,
+      end: _calculatedPages
+    },
+    documents: documentSkeleton
+  }));
 };
